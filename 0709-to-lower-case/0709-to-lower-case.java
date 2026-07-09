3f7296768e5d891b2f93b1bd61d7060a9ae11481
Rupesh-Bhalerao <rupeshbhalerao606@gmail.com>
2026-07-09 22:23:05 +0530

    Time: 1 ms (26.12%), Space: 42.8 MB (67.37%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,22 @@
+// class Solution {
+//     public String toLowerCase(String s) {
+//         return s.toLowerCase();
+//     }
+// }
+class Solution {
+    public String toLowerCase(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+}
