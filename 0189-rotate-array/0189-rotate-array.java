0e059deaebe6362725dc8fa31ad68b3c827652c6
Rupesh-Bhalerao <rupeshbhalerao606@gmail.com>
2026-07-10 17:28:42 +0530

    Time: 2 ms (19.83%), Space: 63 MB (20.14%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -14,3 +14,23 @@ class Solution {
         }
     }
 }
+
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+
+//         int n = nums.length;
+//         k %= n;
+                                                       //for left side rotated
+//         int[] temp = new int[n];
+
+//         // Fill temp array
+//         for (int i = 0; i < n; i++) {
+//             temp[i] = nums[(i - k + n) % n];
+//         }
+
+//         // Copy back
+//         for (int i = 0; i < n; i++) {
+//             nums[i] = temp[i];
+//         }
+//     }
+// }
